733848022b000806a2362904cc5bf58f8cca17d5
Rafael Matoso <rafael.gustavo.gomes@concrete.com.br>
2020-04-29 14:28:09 -0300

    Lesson 20 Completed: structural diretive

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -16,9 +16,10 @@ import { MatListModule } from "@angular/material/list";
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { RedDirective } from './directives/red.directive';
+import { ForDirective } from './directives/for.directive';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent, HomeComponent, ProductCrudComponent, RedDirective],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent, HomeComponent, ProductCrudComponent, RedDirective, ForDirective],
   imports: [
     BrowserModule,
     AppRoutingModule,
