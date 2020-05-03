3282b8acf7cc1058a94128bbfecf4de319acd8bc
Rafael Matoso <rafaelmatosog@gmail.com>
2020-05-02 21:28:03 -0300

    Lesson 37 to 40 Completed

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ProductDeleteComponent } from "./components/product/product-delete/product-delete.component";
 import { ProductUpdateComponent } from "./components/product/product-update/product-update.component";
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
@@ -14,6 +15,7 @@ const routes: Routes = [
   { path: "products", component: ProductCrudComponent },
   { path: "products/create", component: ProductCreateComponent },
   { path: "products/update/:id", component: ProductUpdateComponent },
+  { path: "products/delete/:id", component: ProductDeleteComponent },
 ];
 
 @NgModule({
